825747d615eda0f92d514a2ab1fa135b96919986
shaneboulden <shane.boulden@gmail.com>
2023-07-03 10:53:16 +1000

    Add mitre alignment

--- scripts/generate-stackrox-policy.py
+++ scripts/generate-stackrox-policy.py
@@ -13,13 +13,12 @@ def get_gtfobins(root):
         
 def build_policy(root):
     policy = {}
-    policy['id'] = "4eb78ffe-6a44-4eb0-9e64-f7e926806a96"
     policy['name'] = "GTFOBins component in image"
     policy['description'] = "Alert on deployments with GTFOBins components present"
     policy['rationale'] = "Leaving GTFOBins components inside container images makes it easier for attackers to 'live off the land' inside a container environment"
     policy['remediation'] = "Use your package manager's \"remove\", \"purge\" or \"erase\" command to remove GTFOBins components from the image build for production containers."
     policy['disabled'] = False
-    policy['categories'] = ["Security best practices"]
+    policy['categories'] = ["Security Best Practices"]
     policy['lifecycleStages'] = ["BUILD","DEPLOY"]
     policy['eventSource'] = "NOT_APPLICABLE"
     policy['exclusions'] = []
@@ -46,6 +45,41 @@ def build_policy(root):
 
     policy['policySections'] = [section]
     policy['mitreAttackVectors'] = []
+
+    # TA0002 - Execution. Many of the GTFOBins components enable execution
+    # in various contexts.
+    policy['mitreAttackVectors'].append({
+        "tactic": "TA0002",
+        "techniques": ["T1059.004"]
+    })
+
+    # TA0007 - Discovery. Binaries like dmidecode can be used to provide information
+    # on the environment
+    policy['mitreAttackVectors'].append({
+        "tactic": "TA0007",
+        "techniques": ["T1046"]
+    }) 
+
+    # TA0008 - Lateral movement. Binaries like ssh and netcat can be used 
+    # to enable lateral movement.
+    policy['mitreAttackVectors'].append({
+        "tactic": "TA0008",
+        "techniques": ["T1570"]
+    })
+
+    # TA0010 - Exfiltration. Binaries like tar, curl and tftp can be used to exfiltrate
+    # data from environments.
+    policy['mitreAttackVectors'].append({
+        "tactic": "TA0010",
+        "techniques": ["T1567"]
+    })
+
+    # TA0043 - Reconnaissance. 
+    policy['mitreAttackVectors'].append({
+        "tactic": "TA0043",
+        "techniques": ["T1592"]
+    })
+
     policy['criteriaLocked'] = False
     policy['mitreVectorsLocked'] = False
     policy['isDefault'] = False
