3bf5ee55dfa6446aad5da12829efe13add2c02b8
ValentaA <90060698+ValentaA@users.noreply.github.com>
2023-07-22 20:42:50 +0200

    Create Automated_privilege_escalation_tool.py
    I've started to work on this project with an idea to make it easier for me and the other ones that are learning to become pentesters or the ones 
    with conducting pentests on real targets.
    I hope that there is more people internested in contributing to this project with some new ideas and that we can all work together
    to make it better!

---
+++ Automated_privilege_escalation_tool.py
@@ -0,0 +1,9 @@
+https://github.com/ValentaA/Exploits.git
+
+aspet.py
+MIT Licence
+
+I've started to work on this project with an idea to make it easier for me and the other ones that are learning to become pentesters or the ones 
+with conducting pentests on real targets.
+I hope that there is more people internested in contributing to this project with some new ideas and that we can all work together
+to make it better!
